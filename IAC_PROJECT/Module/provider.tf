5bceef2af4db55bc97e2cac7e304c8dfdfa7a93e
GANESH KUMAR S A <113096975+ganeshdoes@users.noreply.github.com>
2022-10-11 20:44:42 +0530

    Update provider.tf

--- IAC_PROJECT/Module/provider.tf
+++ IAC_PROJECT/Module/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAXX2VOMRJFW3MONWM"
-  secret_key = "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"
+  access_key = "AKIAXX2VOMRJFWONWM"
+  secret_key = "jLvwcgYLN27aU36YiI+ZGolKIODSM0rJ0SUHG9"
 }
