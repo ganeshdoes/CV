a3c771e9116ffecd1ef2fe70fa1af4f7e76da7e9
GANESH KUMAR S A <113096975+ganeshdoes@users.noreply.github.com>
2022-10-11 20:41:53 +0530

    Delete dev directory

--- dev/main.tf
+++
@@ -1,6 +0,0 @@
-module "s3_resource" {
-
-    source = "../Module"
-    demo = "tp1234567890"
-    env = "dev"
-}
--- dev/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "demo" {
-   default = "tp12345"
-   description = "Bucketname"
-}
-
-variable "env" {
-   default = "dev"
-    description = "Environment type"
-}
